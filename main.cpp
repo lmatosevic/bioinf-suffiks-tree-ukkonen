c087aa027ae6df683f9efb67636e5221e78853c3
luka <lukamatosevic5@gmail.com>
2016-01-04 20:48:17 +0100

    Added CMakeLists file and test main program.

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    char name[50];
+    cout << "Enter your name: ";
+    scanf("%s", name);
+    cout << "Hello " << name << endl;
+    return 0;
+}
