343d5fcf94b5951421d573fc181e596d21b1f4d8
luka <lukamatosevic5@gmail.com>
2016-01-13 21:00:37 +0100

    Added classes for reading test files.

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,13 @@
 using std::cout;
 using std::cin;
 using std::flush;
+using std::cerr;
 
-int main() {
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        cerr << "Invalid number of arguments! Please provide test file path." << flush;
+        return 1;
+    }
     cout << "Enter sample string: " << flush;
     cin.getline(T, MAX_LENGTH - 1);
     N = (int) (strlen(T) - 1);
@@ -20,11 +25,11 @@ int main() {
         AddPrefix(active, i);
     }
     dump_edges(N);
-    cout << "Would you like to validate the tree?" << flush;
+    cout << "Do you want to validate generated suffix tree?(y/n)" << flush;
     std::string s;
     getline(cin, s);
     if (s.size() > 0 && s[0] == 'Y' || s[0] == 'y') {
         validate();
     }
-    return 1;
+    return 0;
 }
